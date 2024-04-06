03f943056a648d7911830b104317d79255b901de
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-04-06 14:28:39 +0200

    added render values to squeares

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,10 +4,23 @@ import Square from "../Square";
 
 const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [isValueX, setIsValueX] = useState(true);
+
+  const setSquareValue = (itemSquare) => {
+    const newSquare = square.slice();
+    isValueX ? (newSquare[itemSquare] = "X") : (newSquare[itemSquare] = "O");
+    setIsValueX(!isValueX);
+    setSquare(newSquare);
+  };
+
   return (
     <div className={style.container}>
       {square.map((square, index) => (
-        <Square key={index} value={square} />
+        <Square
+          key={index}
+          value={square}
+          setSquareValue={() => setSquareValue(index)}
+        />
       ))}
     </div>
   );
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,11 @@
 import style from "./style.module.css";
 
-const Square = ({ value }) => {
-  return <div className={style.SquareItem}>{value}</div>;
+const Square = ({ value, setSquareValue }) => {
+  return (
+    <div className={style.SquareItem} onClick={setSquareValue}>
+      {value}
+    </div>
+  );
 };
 
 export default Square;
